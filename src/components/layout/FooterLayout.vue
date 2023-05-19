5134efc20e3841b364e20fa9ab579d763524eb9f
이남곤 <nglee.dev@gmail.com>
2023-05-19 17:53:25 +0900

    feat: footer

---
+++ src/components/layout/FooterLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer class="py-3 my-4 border-top">
+    <div class="container d-flex flex-wrap justify-content-between">
+      <p class="col-md-4 mb-0 text-muted">© 2022 Company, Inc</p>
+      <ul class="nav col-md-4 justify-content-end">
+        <li class="nav-item">
+          <a href="#" class="nav-link px-2 text-muted">Home</a>
+        </li>
+      </ul>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "FooterLayout",
+};
+</script>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,11 +5,13 @@
       <FilterSelection></FilterSelection>
       <SearchResult class="search-result"></SearchResult>
     </main>
+    <FooterLayout></FooterLayout>
   </div>
 </template>
 
 <script>
 import HeaderLayout from "@/components/layout/HeaderLayout.vue";
+import FooterLayout from "@/components/layout/FooterLayout.vue";
 import FilterSelection from "@/components/FilterSelection.vue";
 import SearchResult from "@/components/SearchResult.vue";
 
@@ -17,6 +19,7 @@ export default {
   name: "HomeView",
   components: {
     HeaderLayout,
+    FooterLayout,
     FilterSelection,
     SearchResult,
   },
