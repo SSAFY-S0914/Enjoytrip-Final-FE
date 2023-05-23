49bd8d461c75b6c2f5276e3f4e09399a9213daa7
이남곤 <nglee.dev@gmail.com>
2023-05-23 10:11:06 +0900

    feat: 그룹 만들기 버튼 추가

--- src/views/AllGroupView.vue
+++ src/views/AllGroupView.vue
@@ -2,6 +2,12 @@
   <div>
     <HeaderLayout></HeaderLayout>
     <main class="container">
+      <div class="group-list-header">
+        <h1>전체 그룹</h1>
+        <div>
+          <b-button>새 그룹 만들기</b-button>
+        </div>
+      </div>
       <GroupList></GroupList>
     </main>
     <FooterLayout></FooterLayout>
@@ -23,4 +29,12 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.group-list-header {
+  margin-bottom: 2rem;
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
